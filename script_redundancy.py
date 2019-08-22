17a2d36eb478d684a61bab49d1cffcba7494bc37
Maria Sharabayko <msharabayko@haivision.com>
2019-08-22 15:54:24 +0200

    Added srt-test-live support for redundancy testing. Under dev

--- script_redundancy.py
+++ script_redundancy.py
@@ -13,6 +13,12 @@ from new_processes import Process
 logger = logging.getLogger(__name__)
 
 
+def _nodes_split(ctx, param, value):
+    nodes = list(value)
+    # nodes = [tuple(node.split(':')) for node in nodes]
+    return nodes
+
+
 def generate_buffer():
     return bytearray([(1 + i % 255) for i in range(0, 1315)]) + bytearray([0])
 
@@ -24,15 +30,8 @@ def calculate_interval(bitrate):
         return (1316 * 8) / (bitrate * 1000000)
 
 
-def start_sender(path, ip, port, interval, n):
+def start_sender(args, interval, n):
     logger.info('Starting sender')
-    args = [
-        f'{path}',
-        'file://con',
-        f'srt://{ip}:{port}?nakreport=0&linger=0',
-        # '-v', 
-        # '-loglevel:error'
-    ]
     process = Process('sender', args)
     process.start()
 
@@ -45,7 +44,7 @@ def start_sender(path, ip, port, interval, n):
     try:
         for i in range(0, n):
             if i < 2 or i > 5:
-                logger.debug(f'Sending packet {i + 1}')
+                logger.info(f'Sending packet {i + 1}')
                 time.sleep(interval)
                 buffer[0] = 1 + i % 255
                 process.process.stdin.write(buffer)
@@ -61,15 +60,21 @@ def start_sender(path, ip, port, interval, n):
         process.stop()
 
 
-def start_receiver(path, port, n):
+def read_data(process):
+    is_data_empty = True
+    while is_data_empty:
+        data = process.process.stdout.read(1316)
+
+        if len(data) != 0:
+            is_data_empty = False
+
+        time.sleep(1)
+        print('aha')
+
+    return data
+
+def start_receiver(args, n):
     logger.info('Starting receiver')
-    args = [
-        f'{path}',
-        f'srt://:{port}?nakreport=0&linger=0',
-        'file://con',
-        # '-v', 
-        # '-loglevel:error'
-    ]
     process = Process('receiver', args)
     process.start()
 
@@ -82,10 +87,9 @@ def start_receiver(path, port, n):
 
     try:
         while i < n:
+            logger.info('Waiting for data')
+            data = read_data(process)
             target_values[0] = 1 + i % 255
-
-            logger.debug('Waiting for data')
-            data = process.process.stdout.read(1316)
 
             message = f'Packet {i + 1}, size {len(data)} '
             if target_values == data:
@@ -129,6 +133,8 @@ def cli(debug):
         )
 
 
+# NOTE: This sub-commands are here for srt-live-transmit, better to split 
+# it in a separate file.
 @cli.command()
 @click.option('--ip', default='127.0.0.1', help='IP to call', show_default=True)
 @click.option('--port', default=4200, help='Port to call', show_default=True)
@@ -137,11 +143,18 @@ def cli(debug):
 @click.option('--bitrate', help='Bitrate, Mbit/s', type=float)
 @click.argument('path')
 def sender(ip, port, duration, n, bitrate, path):
+    args = [
+        f'{path}',
+        'file://con',
+        f'srt://{ip}:{port}?nakreport=0&linger=0',
+        # '-v', 
+        # '-loglevel:error'
+    ]
     interval = calculate_interval(bitrate)
     if n is None:
         n = int(duration // interval) + 1
 
-    start_sender(path, ip, port, interval, n)
+    start_sender(args, interval, n)
 
 
 @cli.command()
@@ -151,11 +164,100 @@ def sender(ip, port, duration, n, bitrate, path):
 @click.option('--bitrate', help='Bitrate, Mbit/s', type=float)
 @click.argument('path')
 def receiver(port, duration, n, bitrate, path):
+    args = [
+        f'{path}',
+        f'srt://:{port}?nakreport=0&linger=0',
+        'file://con',
+        # '-v', 
+        # '-loglevel:error'
+    ]
     interval = calculate_interval(bitrate)
     if n is None:
         n = int(duration // interval) + 1
 
-    start_receiver(path, port, n)
+    start_receiver(args, n)
+
+
+@cli.command()
+@click.option(
+    '--port',
+    default=4200,
+    help='Port to listen',
+    show_default=True
+)
+@click.option(
+    '--duration',
+    default=60,
+    help='Duration, s',
+    show_default=True
+)
+@click.option(
+    '--n',
+    help='Number of packets',
+    type=int
+)
+@click.option(
+    '--bitrate',
+    help='Bitrate, Mbit/s',
+    type=float
+)
+@click.argument('path')
+def re_sender(port, duration, n, bitrate, path):
+    args = [
+        f'{path}',
+        'file://con',
+        f'srt://:{port}?groupconnect=true'
+    ]
+    interval = calculate_interval(bitrate)
+    if n is None:
+        n = int(duration // interval) + 1
+
+    print(args)
+
+    start_sender(args, interval, n)
+
+
+@cli.command()
+@click.option(
+    '--node',
+    help='host:port combination, multiple nodes can be defined',
+    required=True,
+    multiple=True,
+    callback=_nodes_split
+)
+@click.option(
+    '--duration',
+    default=60,
+    help='Duration, s',
+    show_default=True
+)
+@click.option(
+    '--n',
+    help='Number of packets',
+    type=int
+)
+@click.option(
+    '--bitrate',
+    help='Bitrate, Mbit/s',
+    type=float
+)
+@click.argument('path')
+def re_receiver(node, duration, n, bitrate, path):
+    args = [
+        f'{path}',
+        f'srt://*?type=redundancy'
+    ]
+    args += node
+    args += [
+        '-g',
+        'file://con'
+    ]
+    interval = calculate_interval(bitrate)
+    if n is None:
+        n = int(duration // interval) + 1
+
+    print(args)
+    start_receiver(args, n)
 
 
 if __name__ == '__main__':
